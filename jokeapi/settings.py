bfa292f1521e248cfd4d1c65e7f886927511669d
Vipul  Ghadi <99540970+vipulghadi@users.noreply.github.com>
2023-12-25 01:30:48 +0530

    Update settings.py

--- jokeapi/settings.py
+++ jokeapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9uu*l&@rj4&wrs)r&hp&((3ew381&3x&w3^z3vy8b1r-(2vxae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
